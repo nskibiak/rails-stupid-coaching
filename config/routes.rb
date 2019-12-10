1192295ecd7912ec4cdac36f735aafe4a84b98cf
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-10 19:39:25 +0900

    added pages controller with ask and answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/ask'
+  get 'pages/answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
