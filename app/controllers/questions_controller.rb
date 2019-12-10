71884344a76b7ac031d3c8ed6837a781bcb681ab
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-10 20:14:52 +0900

    added coach response and logic to controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:query]
+    @query = params[:query]
+
+    if @query.downcase == 'i am going to work'
+      @coach = 'Great!'
+    elsif @query[-1] == '?'
+      @coach = 'Silly question, get dressed and go to work!'
+    else
+      @coach = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise she/he will answer I don't care, get dressed and go to work!
