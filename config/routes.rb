33efe6057adb709adec8e7e432af9d39a95e453f
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-10 20:17:31 +0900

    added nicknames for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask'
-  get 'ask?', to: 'questions#answer'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'ask?', to: 'questions#answer', as: :answer
   get 'answer', to: 'questions#answer'
 end
